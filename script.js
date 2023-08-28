35661973555604b2c3bf4d28426bd91b42812205
Brendan <brendanjlee5@gmail.com>
2023-08-29 00:22:01 +0900

    add and change status button functionality

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-const myLibrary = [];
+// Global Variables
+const table = document.querySelector('.book-table');
+const form = document.querySelector('.book-form');
 
+/*  Library Functions   */ 
 function Book(title, author, pages, haveRead) {
     this.title = title;
     this.author = author;
@@ -7,30 +10,91 @@ function Book(title, author, pages, haveRead) {
     this.haveRead = haveRead;
 }
 
-function addBookToLibrary(book) {
-    myLibrary.push(book);
+// delete book from library
+function deleteBook(book, library) {
+    console.log(library)
+    for (let i = 0; i < library.length; i++) {
+        if (isEqual(book, library[i])) {
+            library.splice(i, 1);
+            return true;
+        }
+    }
+    return false;
 }
 
-function showLibrary() {
+// equates two books
+function isEqual(book1, book2) {
+    return (book1.title === book2.title &&
+        book1.author === book2.author &&
+        book1.pages == book2.pages);
+}
+
+// Find all books by title
+function findBookByTitle(bookTitle, library) {
+    for (let i = 0; i < library.length; i++) {
+        if (library[i].title === bookTitle) return library[i];
+    }
+    return null;
+}
+
+// Find book by titleAuthor toString
+function findBookByString(titleAuthor, library) {
+    // replace underscores with space
+    let title = titleAuthor.split('-')[0].replace('_', ' ');    
+    let author = titleAuthor.split('-')[1].replace('_', ' ');
+    for (let i = 0; i < library.length; i++) {
+        let currBook = library[i];
+        if (currBook.title == title && currBook.author == author) return currBook;
+    }
+    return null;
+}
+
+function convertBookToString(book) {
+    let bookString = `${book.title}-${book.author}`;
+    // replace whitespace with underscore
+    return bookString.split(' ').join('_');
+}
+
+
+/*  Rednering Functions  */
+const myLibrary = [
+    new Book('JK Rowlilng', 'Harry Potter', 399, true),
+    new Book('Dune', 'Frank Herbert', 1000, false),
+    new Book('1984', 'George Orwell', 200, true),
+];
+
+// Render all books in the library
+function render() {
     myLibrary.forEach((book) => {
-        console.log(book);
+        renderBook(book);
     })
 }
 
-let i = 0;
-// populate an entry on table
-function populateTable(book) {
-    // create a reference to the table
-    const table = document.querySelector('.book-table');
+// Clear the Table
+function clearTable() {
+    while(table.childNodes.length > 2) {
+        table.removeChild(table.lastChild);
+    }
+}
 
+// populate an entry on table
+function renderBook(book) {
     // create a new row
     let tr = document.createElement(`tr`);
+
+    // add data class to the row
+    tr.setAttribute(`data-book`, `${convertBookToString(book)}`);
 
     // create entries
     let title = document.createElement('td');
     let author = document.createElement('td');
     let pages = document.createElement('td');
-    let haveRead = document.createElement('td');
+    let readButton = document.createElement('button');
+    readButton.classList.add('book-status');
+    let deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('delete-btn');
+    deleteBtn.innerHTML = "Delete"
+
 
     // populate entry
     title.appendChild(document.createTextNode(book.title));
@@ -38,23 +102,27 @@ function populateTable(book) {
     pages.appendChild(document.createTextNode(book.pages));
 
     if (book.haveRead) {
-        haveRead.appendChild(document.createTextNode('Read'));
+        readButton.innerHTML = 'Read';
     } else {
-        haveRead.appendChild(document.createTextNode('Not Read'));
+        readButton.innerHTML = 'Not Read';
     }
 
     // add to table
     tr.appendChild(title)
     tr.appendChild(author)
     tr.appendChild(pages)
-    tr.appendChild(haveRead)
-
+    tr.appendChild(readButton);
+    tr.appendChild(deleteBtn);
     table.appendChild(tr);
+
+    // event listeners
+    readButton.addEventListener('click', changeBookStatus);
+
+    deleteBtn.addEventListener('click', removeBookFromTable);
 } 
 
-const form = document.querySelector('.book-form');
-form.addEventListener('submit', bookSubmit);
 
+// adds book to the library and renders the book 
 function bookSubmit(e) {
     e.preventDefault();
 
@@ -64,13 +132,31 @@ function bookSubmit(e) {
     let haveRead = document.getElementById('have-read');
 
     const newBook = new Book(title.value, author.value, numPages.value, haveRead.checked);
-
-    populateTable(newBook)
+    
+    myLibrary.push(newBook);
+    renderBook(newBook);
     form.reset();
 }
 
+function changeBookStatus(e) {
+    let bookString = e.currentTarget.parentNode.dataset.book;
+    let currBook = findBookByString(bookString, myLibrary);
+    for (let i = 0; i < myLibrary.length; i++) {
+        if (isEqual(currBook, myLibrary[i])) {
+            myLibrary[i].haveRead = !myLibrary[i].haveRead;
+        }
+    }
+    clearTable();
+    render();
+}
 
-// runners
-const exampleBook = new Book('Joe', 'Joe Joe', 1323, true);
-populateTable(exampleBook)
-populateTable(new Book('Keith', 'Harring', 12312, true));
+function removeBookFromTable(e) {
+    let bookString = e.currentTarget.parentNode.dataset.book;
+    deleteBook(findBookByString(bookString, myLibrary), myLibrary);
+    clearTable();
+    render();
+}
+
+// Event Listeners
+form.addEventListener('submit', bookSubmit);
+render();
